9dd36b2692ceba5cf08dd2f728784416da3e5fa8
rjwadley <robbiethewadley@gmail.com>
2019-01-18 17:59:03 -0500

    pwa icon

--- scripts/app.js
+++ scripts/app.js
@@ -8,13 +8,25 @@ var app = new Vue({
   },
   methods: {  run: function () {
     var self = this;
-    self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
+    self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5, mirror: false });
     self.scanner.addListener('scan', function (content, image) {
       self.scans.unshift({ date: +(Date.now()), content: content });
+      
+   	  $(".sidebar").addClass("sidebar-show");
+setTimeout(
+  function() 
+  {
+    $(".sidebar").removeClass("sidebar-show");
+  }, 3000);
+  
     });
+    
     Instascan.Camera.getCameras().then(function (cameras) {
       self.cameras = cameras;
-      if (cameras.length > 0) {
+      if (cameras.length === 2) {
+        self.activeCameraId = cameras[1].id;
+        self.scanner.start(cameras[1]);
+      } else if (cameras.length > 0) {
         self.activeCameraId = cameras[0].id;
         self.scanner.start(cameras[0]);
       } else {
