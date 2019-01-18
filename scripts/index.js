9afd6db20c70055e8ee2a66a3b4caea60c74ae5e
rjwadley <robbiethewadley@gmail.com>
2019-01-18 18:18:26 -0500

    web apps

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,31 @@
 /*eslint-env jquery */
+
 $("document").ready(function() {
+	
 $('.qr-start').click(function() {
 	$('.qr-container').show();
 	app.run();
+	history.pushState({}, 'QR');
 });
 $('.qr-stop').click(function() {
 	$('.qr-container').hide();
 	app.stop();
 });
+
+
+
 });
+
+//This is the "Offline copy of pages" service worker
+
+//Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+if (navigator.serviceWorker.controller) {
+  console.log('[PWA Builder] active service worker found, no need to register')
+} else {
+  //Register the ServiceWorker
+  navigator.serviceWorker.register('pwabuilder-sw.js', {
+    scope: './'
+  }).then(function(reg) {
+    console.log('Service worker has been registered for scope:'+ reg.scope);
+  });
+}
