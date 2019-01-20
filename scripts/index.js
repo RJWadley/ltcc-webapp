ada6706ca9242358a6b1f4e2bb108825a3e70099
rjwadley <robbiethewadley@gmail.com>
2019-01-20 17:57:07 -0500

    home page improvements

--- scripts/index.js
+++ scripts/index.js
@@ -4,9 +4,13 @@ $.mobile.autoInitializePage = false;
 
 $("document").ready(function() {
 
-if (!/Mobi|Android/i.test(navigator.userAgent)) {
-			$(".install-prompt").hide();
-}
+	if (!/Mobi|Android/i.test(navigator.userAgent)) {
+		$(".install-prompt").hide();
+	}
+
+	if (window.matchMedia('(display-mode: standalone)').matches) {
+		$(".install-prompt").hide();
+	}
 
 	$('.qr-start').click(function() {
 		$('.qr-container').show();
@@ -38,14 +42,20 @@ if (!/Mobi|Android/i.test(navigator.userAgent)) {
 
 	$(".installApp").click(function() {
 		$(".install-prompt").fadeOut();
-		
-		    // iOS detection from: http://stackoverflow.com/a/9039885/177710
-    if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
+
+		// iOS detection from: http://stackoverflow.com/a/9039885/177710
+		if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
 			$(".ios-install-prompt").slideDown();
-    } else {
-    	$(".android-install-prompt").slideDown();
-    }
-		
+		} else {
+			$(".android-install-prompt").slideDown();
+		}
+
+		setTimeout(
+			function() {
+				$(".android-install-prompt").fadeOut();
+				$(".ios-install-prompt").fadeOut();
+			}, 8000);
+
 	});
 
 	$(".installPromptClose").click(function() {
@@ -53,13 +63,18 @@ if (!/Mobi|Android/i.test(navigator.userAgent)) {
 
 	});
 
-	jQuery(".ltcc-logo").on("swipedown", function(event) {
+	jQuery(".ltcc-logo").on("swipedown", function() {
 		screenfull.exit();
 		if (window.matchMedia('(display-mode: standalone)').matches) {
+			//do nothing
+		} else if (/Mobi|Android/i.test(navigator.userAgent)) {
+			// mobile!
 			$(".install-prompt").show();
+
 		}
-
-
 	});
+
+	$('.preloader').fadeOut();
+	$(".global-content").show();
 
 });
