e563736b18ba12727a4702ca79b3e0b419ae11a0
rjwadley <robbiethewadley@gmail.com>
2019-01-19 23:43:39 -0500

    mobile improvements

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,55 @@
 /*eslint-env jquery */
 
-$("document").ready(function() {
-	
-$('.qr-start').click(function() {
-	$('.qr-container').show();
-	app.run();
-	history.pushState({}, 'QR');
-});
-$('.qr-stop').click(function() {
-	$('.qr-container').hide();
-	app.stop();
-});
+$.mobile.autoInitializePage = false;
 
+$("document").ready(function() {
+
+if (!/Mobi|Android/i.test(navigator.userAgent)) {
+			$(".install-prompt").hide();
+}
+
+	$('.qr-start').click(function() {
+		$('.qr-container').show();
+		app.run();
+		history.pushState({}, 'QR');
+	});
+	$('.qr-stop').click(function() {
+		$('.qr-container').hide();
+		app.stop();
+	});
+
+	$('.goFullScreen').click(() => {
+		if (screenfull.enabled) {
+			screenfull.request();
+		} else {
+			alert("unable to fullscreen");
+		}
+		if (/Mobi|Android/i.test(navigator.userAgent)) {
+			// mobile!
+			$(".exit-fullscreen-prompt").slideDown();
+			setTimeout(
+				function() {
+					$(".exit-fullscreen-prompt").fadeOut();
+				}, 3000);
+		}
+
+		$(".install-prompt").hide();
+	});
+
+	$(".installApp").click(function() {
+		addToHomescreen();
+	});
+
+	$(".installPromptClose").click(function() {
+		$(".install-prompt").fadeOut();
+
+	});
+
+	jQuery(".ltcc-logo").on("swipedown", function(event) {
+		screenfull.exit();
+		$(".install-prompt").show();
+
+	});
 
 
 });
