5e4a61c8afbce8bfb52c4108785c2001ed3963c1
Robert Wadley <robbiethewadley@gmail.com>
2019-02-26 03:08:23 +0000

    fix camera

--- instascan/app.js
+++ instascan/app.js
@@ -8,22 +8,14 @@ var app = new Vue({
   },
   mounted: function () {
     var self = this;
-    self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5, mirror: false });
+    self.scanner = new Instascan.Scanner({   mirror: false, video: document.getElementById('preview'), scanPeriod: 5 });
     self.scanner.addListener('scan', function (content, image) {
       self.scans.unshift({ date: +(Date.now()), content: content });
-      
-   	  $(".sidebar").addClass("sidebar-show");
-setTimeout(
-  function() 
-  {
-    $(".sidebar").removeClass("sidebar-show");
-  }, 3000);
-  
+      window.location.replace(content);
     });
-    
     Instascan.Camera.getCameras().then(function (cameras) {
       self.cameras = cameras;
-      if (cameras.length === 2) {
+      if (cameras.length = 2) {
         self.activeCameraId = cameras[1].id;
         self.scanner.start(cameras[1]);
       } else if (cameras.length > 0) {
@@ -36,17 +28,13 @@ setTimeout(
       console.error(e);
     });
   },
+  methods: {
     formatName: function (name) {
       return name || '(unknown)';
     },
     selectCamera: function (camera) {
       this.activeCameraId = camera.id;
       this.scanner.start(camera);
-    },
-    start: function() {
-    },
-    stop: function() {
-   	  this.scanner.stop();
     }
-  
+  }
 });
